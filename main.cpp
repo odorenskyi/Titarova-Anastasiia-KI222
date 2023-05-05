6d844dfc42bbb6ed4a73dab10d30c975dc4c78db
Анастасія Тітарова <124300275+anastasiatitarova@users.noreply.github.com>
2023-05-05 15:27:10 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,32 +1,25 @@
 #include <iostream>
+
 using namespace std;
 
+int count_zeros(int n) {
+    int count = 0;
+    while (n > 0) {
+        if ((n & 1) == 0) {
+            count++;
+        }
+        n >>= 1;
+    }
+    return count;
+}
+
 int main() {
-    const int WIND_SPEEDS[13] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-    const string WIND_DESCRIPTIONS[13] = {"Calm", "Light air", "Light breeze", "Gentle breeze", "Moderate breeze", "Fresh breeze", "Strong breeze", "Near gale", "Gale", "Strong gale", "Storm", "Violent storm", "Hurricane"};
-
-    int wind_speed;
-    int min_wind_speed = INT_MAX;
-
-    for (int i = 0; i < 24; i++) {
-        cout << "Enter the wind speed for hour " << i+1 << ": ";
-        cin >> wind_speed;
-
-        if (wind_speed < min_wind_speed) {
-            min_wind_speed = wind_speed;
-        }
-    }
-
-    int beaufort_scale = 0;
-
-    for (int i = 0; i < 13; i++) {
-        if (min_wind_speed <= i*2) {
-            beaufort_scale = WIND_SPEEDS[i];
-            break;
-        }
-    }
-
-    cout << "The minimum wind speed for the day is " << min_wind_speed << " m/s (" << WIND_DESCRIPTIONS[beaufort_scale] << " on the Beaufort scale)." << endl;
-
+    int n;
+    cout << "Enter a natural number between 0 and 500700: ";
+    cin >> n;
+    int d1 = (n & 1);
+    int num_zeros = (d1 == 0) ? count_zeros(n) : 32 - count_zeros(n);
+    cout << "Number of binary zeros: " << num_zeros << endl;
     return 0;
 }
+
